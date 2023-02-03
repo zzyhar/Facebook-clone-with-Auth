7539cdfea383d7ed79d834a3279774ffdd0cc9b5
zzyhar <93952085+zzyhar@users.noreply.github.com>
2023-02-03 17:57:15 -0500

    Register, Login Pages, and From

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,5 +1,32 @@
+import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
+import Form from "./Form";
 const LoginPage = () => {
-  return <div>loginpage</div>;
+  const theme = useTheme();
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  return (
+    <Box
+      width="100%"
+      backgroudColor={theme.palette.background.alt}
+      p="1rem 6%"
+      textAlign="center"
+    >
+      <Typography fontWeight="bold" fontSize="32px" color="primary">
+        Sociopedia
+      </Typography>
+      <Box
+        width={isNonMobileScreens ? "50%" : "93%"}
+        p="2rem"
+        m="2rem auto"
+        borderRadius="1.5rem"
+        backgroudColor={theme.palette.background.alt}
+      >
+        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+          Welcome to Socialpedia, the Social media for Sociopaths!
+        </Typography>
+        <Form />
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;
